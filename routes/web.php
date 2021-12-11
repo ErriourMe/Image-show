3db23306557e301ea8d4349601bb690743a56b7d
eleyandi <gonchar.vladimir@tftc.dev>
2021-12-11 09:40:26 +0300

    Fix

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Route::get('/makeimage', 'ImageController@index');
 Route::get('/gd', 'GdController@index');
-Route::get('/imagick', 'Imagick@index');
+Route::get('/imagick', 'ImagickController@index');
 Route::get('/login', function () {
     return 'erriour';
 });
